e3f68862616f29da206f97051153cb83715febf7
Uditha Anuradha Gurubawila <udithaanuradha276@gmail.com>
2026-04-27 09:04:43 +0530

    update supervisor selection branch

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ console.log('✅ Cloudinary configured for file uploads');
 const db = mysql.createPool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT || 4000,
+
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
